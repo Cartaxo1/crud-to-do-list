c573fd15e6fdce4897e40c4e14f254ec81db1aaa
Cartaxo1 <cartaxojoao1@gmail.com>
2024-09-05 17:01:49 -0300

    nav comp styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavComponent } from "./components/nav/nav.component";
+import { NavComponent } from './components/nav/nav.component';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, ButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'crud-to-do-list';
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
