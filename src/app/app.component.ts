f2e2be08be94abed1642c06dc8539271f0c26326
Cartaxo1 <cartaxojoao1@gmail.com>
2024-09-06 17:07:44 -0300

    create modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { ButtonModule } from 'primeng/button';
+import { HomeComponent } from "./components/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, ButtonModule],
+  imports: [RouterOutlet, NavComponent, ButtonModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+
 import { NavComponent } from "../nav/nav.component";
 import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, NavComponent, ButtonModule],
+  imports: [
+    RouterModule,
+    NavComponent,
+    ButtonModule,
+    DialogModule,
+    InputTextModule,
+    CardModule
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  visible: boolean = false;
 
+  showDialog() {
+    this.visible = true;
+  }
 }
+
+
