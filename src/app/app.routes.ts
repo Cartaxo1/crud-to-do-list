a1e2276fabf5f321fd7e3be38f8900f5b39ef096
Cartaxo1 <cartaxojoao1@gmail.com>
2024-09-10 10:36:58 -0300

    create route task

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
   },
+  {
+    path: 'task',
+    component: TasksComponent
+  }
 ];
 export class AppRoutes {}
