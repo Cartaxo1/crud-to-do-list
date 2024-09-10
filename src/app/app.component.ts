92a172b6ea9533f2775eb9b577c8c6802d19a8c5
Cartaxo1 <cartaxojoao1@gmail.com>
2024-09-10 12:55:21 -0300

    create drag and drop tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { ButtonModule } from 'primeng/button';
 import { HomeComponent } from "./components/home/home.component";
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, ButtonModule, HomeComponent, RouterLink],
+  imports: [RouterOutlet, NavComponent, ButtonModule, HomeComponent, RouterLink, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,12 +1,69 @@
 import { Component } from '@angular/core';
 
+import { TasksService } from '../../services/tasks.service';
+import { Task } from '../../models/tasks.model';
+import { DragDropModule } from 'primeng/dragdrop';
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [DragDropModule],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.scss'
+  styleUrl: './tasks.component.scss',
 })
 export class TasksComponent {
+  tasks: Task[] = [];
+  selectedTasks: Task[] = [];
+  draggedTask: Task | undefined | null;
 
+  constructor(private tasksService: TasksService) {}
+
+  getTasks() {
+    this.tasksService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks;
+      console.log(this.tasks);
+    });
+  }
+  ngOnInit() {
+    this.selectedTasks = [];
+    this.tasks = [
+      {
+        id: 1,
+        created_at: '2022-11-01T00:00:00.000Z',
+        title: 'Tarefa 1',
+        description: 'Descrição da tarefa 1',
+        status: false,
+      },
+    ];
+  }
+
+  dragStart(task: Task) {
+    this.draggedTask = task;
+  }
+
+  drop() {
+    if (this.draggedTask) {
+      let draggedProductIndex = this.findIndex(this.draggedTask);
+      this.selectedTasks = [
+        ...(this.selectedTasks as Task[]),
+        this.draggedTask,
+      ];
+      this.tasks = this.tasks?.filter((val, i) => i != draggedProductIndex);
+      this.draggedTask = null;
+    }
+  }
+
+  dragEnd() {
+    this.draggedTask = null;
+  }
+
+  findIndex(task: Task) {
+    let index = -1;
+    for (let i = 0; i < (this.tasks as Task[]).length; i++) {
+      if (task.id === (this.tasks as Task[])[i].id) {
+        index = i;
+        break;
+      }
+    }
+    return index;
+  }
 }
