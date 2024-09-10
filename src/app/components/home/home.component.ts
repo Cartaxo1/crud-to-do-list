8f0c66e7c727efba747a3a87708c33f9a72ed29e
Cartaxo1 <cartaxojoao1@gmail.com>
2024-09-09 22:53:27 -0300

    api commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { NavComponent } from "../nav/nav.component";
+import { NavComponent } from '../nav/nav.component';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
 import { CardModule } from 'primeng/card';
+import { TasksService } from '../../services/tasks.service';
+import { Task } from '../../models/tasks.model';
 
 @Component({
   selector: 'app-home',
@@ -16,7 +18,7 @@ import { CardModule } from 'primeng/card';
     ButtonModule,
     DialogModule,
     InputTextModule,
-    CardModule
+    CardModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
@@ -24,9 +26,22 @@ import { CardModule } from 'primeng/card';
 export class HomeComponent {
   visible: boolean = false;
 
+  tasks: Task[] = [];
+
+  constructor(private tasksService: TasksService) {}
+
   showDialog() {
     this.visible = true;
   }
+
+  getTasks() {
+    this.tasksService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks;
+      console.log(this.tasks);
+    });
+  }
+
+  ngOnInit() {
+    this.getTasks();
+  }
 }
-
-
--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,10 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Task } from '../models/tasks.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TasksService {
-
-  constructor(private http: HttpClient) { }
+  private apiUrl = 'https://qvelvlmqlacumhiuywwm.supabase.co';
+  private apiKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2ZWx2bG1xbGFjdW1oaXV5d3dtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU0NzU2MTAsImV4cCI6MjA0MTA1MTYxMH0.4b1w3C0zmwiyzETNhRFSidL4ao14bM1F-3wtTxACwP0';
+  constructor(private http: HttpClient) {}
+ getTasks() {
+    return this.http.get<Task[]>(`${this.apiUrl}/rest/v1/toDoList`, {
+      headers: {
+        apikey: this.apiKey,
+        Authorization: `Bearer ${this.apiKey}`,
+      },
+    });
+  }
 }
