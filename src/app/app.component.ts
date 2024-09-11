e3415b4ad537f460c5d093b890b5929486fce859
Cartaxo1 <cartaxojoao1@gmail.com>
2024-09-10 21:34:07 -0300

    hotfix routerlink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { ButtonModule } from 'primeng/button';
 import { HomeComponent } from "./components/home/home.component";
@@ -9,7 +9,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, ButtonModule, HomeComponent, RouterLink, CommonModule],
+  imports: [RouterOutlet, NavComponent, ButtonModule, HomeComponent, RouterLink, CommonModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule,RouterModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
